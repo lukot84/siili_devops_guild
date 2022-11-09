7bb1184d2d319bf1c05b43a289296988114482df
Lukasz Kot <lukaszkot@INV-2643.local>
2022-11-09 09:22:15 +0100

    update

--- ec2guard_Michal.sh
+++ ec2guard_Michal.sh
@@ -6,18 +6,12 @@ EC2_IP_ADDRESS=$(echo `hostname` \
     | tr - . \
     )
 
-# EC2_IP_ADDRESS:
-# 172.31.26.182
-
 # Get the instance ID:
 EC2_INSTANCE_ID=$(aws ec2 describe-instances \
     --filter Name=private-ip-address,Values=$(echo "${EC2_IP_ADDRESS}") \
     --query 'Reservations[].Instances[].InstanceId' \
     --output text \
     )
-
-# EC2_INSTANCE_ID:
-# i-0cb81d37720eb0c59
 
 # Get a list of all tags assigned to this EC2 machine:
 TAG_LIST=$(aws ec2 describe-instances \
@@ -26,43 +20,20 @@ TAG_LIST=$(aws ec2 describe-instances \
     --output json \
     )
 
-# TAG_LIST:
-#[
-#    {
-#        "Key": "Type",
-#        "Value": "jenkins"
-#    },
-#    {
-#        "Key": "Owner",
-#        "Value": "michal.lusiak@siili.com"
-#    },
-#    {
-#        "Key": "Name",
-#        "Value": "EC2-ML-Jenkins-Master"
-#    }
-#]
-
 # From the above list filter down the "Name" of the EC2:
 TAG_EC2_NAME=$(echo "${TAG_LIST}" \
     | jq -r '.[] | select(.Key | contains("Name")) | .Value' \
     )
-
-# TAG_EC2_NAME:
-# EC2-ML-Jenkins-Master
 
 # And the "Owner":
 TAG_EC2_OWNER=$(echo "${TAG_LIST}" \
     | jq -r '.[] | select(.Key | contains("Owner")) | .Value' \
     )
 
-# TAG_EC2_OWNER:
-# michal.lusiak@siili.com
-
 # Now, check if the AWS "Owner" TAG was set:
 if [ ! -z "${TAG_EC2_OWNER}" ]; then
     # Get the name only form provided email address:
     OWNER_NAME=$(echo "${TAG_EC2_OWNER}" | sed 's/@siili.com//')
-    echo "${OWNER_NAME}"
 else
     # The "Owner" AWS TAG was not set, so will communicate that:
     TAG_EC2_OWNER="unknown! :alert:"
@@ -84,14 +55,10 @@ function SendSlackMessage() {
     fi
 }
 
-CURRENT_UPTIME="2"
-TIME_LEFT="58"
-
 UPTIME_LIMIT=240
 UPTIME_SECONDS=$(cat /proc/uptime | grep -o '^[0-9]\+')
 UPTIME_MINUTES=$((${UPTIME_SECONDS} / 60))
 UPTIME_DIFF=$((${UPTIME_LIMIT} - ${UPTIME_MINUTES}))
-
 
 # Check whether it should say "? minute" or "? minutes":
 [[ "${CURRENT_UPTIME}" -gt 1 ]] && S_FOR_CURRENT_MINUTES="s" || S_FOR_CURRENT_MINUTES=""
